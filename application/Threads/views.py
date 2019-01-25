6dff223a6d9c04155dd66b71663ff4388aeb1337
Erkka Rahikainen <Erkka.Rahikainen@gmail.com>
2019-01-25 22:15:22 +0200

    Trying to add voting functionality

--- application/Threads/views.py
+++ application/Threads/views.py
@@ -2,13 +2,15 @@ from application import app, db
 from flask import render_template, request, redirect, url_for
 from application.Threads.Thread import Thread
 
+
 @app.route("/Threads/new/")
 def Threads_form():
     return render_template("Threads/new.html")
 
+
+##Create a new Thread
 @app.route("/Threads/", methods=["POST"])
 def Threads_create():
-
     t = Thread(request.form.get("name"))
 
     db.session().add(t)
@@ -16,6 +18,16 @@ def Threads_create():
 
     return redirect(url_for("Threads_index"))
 
+
 @app.route("/Threads", methods=["GET"])
 def Threads_index():
-    return render_template("Threads/list.html", Threads = Thread.query.all())
+    return render_template("Threads/list.html", Threads=Thread.query.all())
+
+
+@app.route("/Thread/<Thread_id>/", methods=["POST"])
+def Thread_vote(Thread_id):
+    t = Thread.query.get(Thread_id)
+    t.votes += 1
+    db.session().commit()
+
+    return redirect(url_for("Threads_index"))
